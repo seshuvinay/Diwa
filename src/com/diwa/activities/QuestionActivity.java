7428f044f2f72aec20f431e47339bff2d457f085
seshuvinay <seshu.vinay@gmail.com>
2013-11-10 10:41:01 +0530

    Few Imp Changes

---
+++ src/com/diwa/activities/QuestionActivity.java
@@ -0,0 +1,18 @@
+package com.diwa.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuestionActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_question);
+		
+		
+		
+	}
+	
+
+}
